f66f39473067b7662ff8ad6ac61a918e72f861a3
Joan R <jramos@Joans-Air.home>
2020-04-05 19:03:13 +0200

    test

--- vars/test.groovy
+++ vars/test.groovy
@@ -9,6 +9,8 @@ def call(String REPO, String PROJECT) {
                 steps {
                     withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
                       sh 'echo Build'
+                      sh 'echo aaa ${REPO}'
+                      sh 'echo bbb ${PROJECT}'
                       sh 'apt update && apt install -y awscli docker.io'
                       sh 'aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin ${REPO}/${PROJECT}'
                       sh 'docker build -t test .'
