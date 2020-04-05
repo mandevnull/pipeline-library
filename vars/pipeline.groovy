9bde727dc23630fb8365b05ac14791adf21c6d07
Joan R <jramos@Joans-Air.home>
2020-04-05 13:43:47 +0200

    Add pipeline

---
+++ vars/pipeline.groovy
@@ -0,0 +1,20 @@
+def call() {
+pipeline {
+    agent none
+    stages {
+      stage('Build & Test') {  agent { label 'aws' }
+        steps {
+           sh 'echo hola > hola.txt'
+           sh 'ls'
+            stash name: "first-stash", includes: "*.txt"
+              }
+      }
+        stage ('check') {   agent { label 'aws2' }
+            steps {
+                unstash "first-stash"
+             sh 'ls'
+            }
+        }
+    }
+ }
+}
