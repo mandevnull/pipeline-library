a39e7edc59e5732932d8ae347eaa6a95a42d617b
mandevnull <32055001+mandevnull@users.noreply.github.com>
2020-04-05 20:15:48 +0200

    Update test.groovy

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,4 +1,4 @@
-def call(REPO) {
+def call(String REPO) {
     pipeline {
         agent none
         stages {
@@ -9,7 +9,7 @@ def call(REPO) {
                 steps {
                     withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
                       sh 'echo Build'
-                      sh 'echo aaa ${REPO}'
+                      echo $REPO
                       sh 'echo bbb ${PROJECT}'
                       sh 'apt update && apt install -y awscli docker.io'
                       sh 'aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin ${REPO}/${PROJECT}'
