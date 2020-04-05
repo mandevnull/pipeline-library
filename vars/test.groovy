3474f55f753c5f9d006d5a6c14fda80905ba7c35
Joan R <jramos@Joans-Air.home>
2020-04-05 17:51:33 +0200

    test aws creds

--- vars/test.groovy
+++ vars/test.groovy
@@ -1,20 +1,31 @@
-def call() {
-pipeline {
-    agent none
-    stages {
-      stage('Build & Test') {  agent { label 'aws' }
-        steps {
-           sh 'echo hola > hola.txt'
-           sh 'ls'
-            stash name: "first-stash", includes: "*.txt"
-              }
-      }
-        stage ('check') {   agent { label 'aws2' }
-            steps {
-                unstash "first-stash"
-             sh 'ls'
+aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin 179081257421.dkr.ecr.eu-west-1.amazonaws.com/test
+
+def call(def REPO, def PROJECT) {
+    pipeline {
+        agent none
+        stages {
+            stage('Build') {  
+                agent { 
+                    label 'aws' 
+                }
+                steps {
+                    withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', accessKeyVariable: 'AWS_ACCESS_KEY_ID', credentialsId: 'aws', secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+                    sh 'echo Build'
+                    sh 'apt update && apt install -y awscli docker'
+                    sh 'aws ecr get-login-password --region eu-west-1 | docker login --username AWS --password-stdin ${REPO}/${PROJECT}'
+                    sh 'docker build -t test .'
+                    sh 'docker tag test:latest ${REPO}/${PROJECT}:${BUILD_NUMBER}'
+                    sh 'docker push ${REPO}/${PROJECT}:${BUILD_NUMBER}'
+                }
+            }
+            stage ('check') {   
+                agent { 
+                    label 'aws2' 
+                }
+                steps {
+                    sh 'ls'
+                }
             }
         }
     }
- }
 }
